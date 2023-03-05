5e9e33b3280ac24349b2e66f2776760353679522
cjrincon <caterinela1@gmail.com>
2023-03-05 10:27:03 -0500

    function correction, delete pkgdown

--- R/split_seq.R
+++ R/split_seq.R
@@ -2,7 +2,7 @@
 #'
 #' @author Catherine Rincon, \email{catherine.rincon@udea.edu.co}
 #'
-#' @param dataset Dataset to split into samples as univariate dataset
+#' @param dataset Dataset to split into samples as multivariate sequences dataset
 #' @param n_steps Number of steps to split into samples
 #'
 #' @return Returns two data sets: one with the information of the x
@@ -11,12 +11,12 @@
 #'
 #'
 #' @export
-split_unv <- function(dataset, n_steps){
+split_seq <- function(dataset, n_steps){
   for (i in 1:nrow(dataset)){
     end_ix <- i + n_steps - 1
     if (end_ix > nrow(dataset)){break}
     seq_x <- dataset[i:end_ix, -ncol(dataset)]
-    eq_y <- dataset[end_ix, ncol(dataset)]
+    seq_y <- dataset[end_ix, ncol(dataset)]
     if (i==1){
       x <- seq_x
       y <- seq_y}
@@ -24,7 +24,7 @@ split_unv <- function(dataset, n_steps){
       x <- c(x, seq_x)
       y <- c(y, seq_y)}}
 
-  x <- array(X, dim = c(nrow(dataset)-(n_steps)+1, n_steps, ncol(dataset)-1))
+  x <- array(x, dim = c(nrow(dataset)-(n_steps)+1, n_steps, ncol(dataset)-1))
   y <- array(y)
 
   list('x'= x, 'y' = y)
