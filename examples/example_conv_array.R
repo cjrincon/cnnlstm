800cc9f4d70d46645b8ef9cf71f6aa84cf8e6e8e
cjrincon <caterinela1@gmail.com>
2023-11-30 20:05:40 -0500

    example conv_array data_NASA

--- examples/example_conv_array.R
+++ examples/example_conv_array.R
@@ -6,3 +6,10 @@ data <- data.frame(
   'cost' = c(23, 10, 15, 25),
   'number' = c(1.2, 3.4, 4.5, 5.6))
 conv_array(data)
+
+
+
+# Example 2
+# Generating an array with the data: NASA
+data(data_NASA)
+data_NASA <- conv_array(data_NASA)
